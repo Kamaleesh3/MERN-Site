f870620382543669db3112e041def1896833280f
Kamaleesh3 <161963087+Kamaleesh3@users.noreply.github.com>
2024-04-28 20:51:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config({path:path.join(__dirname,"config/config.env")});
-const cors = require('cors');
+const cors = require('cors')
 
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname,'uploads')))
@@ -15,13 +15,13 @@ app.use('/uploads', express.static(path.join(__dirname,'uploads')))
 const products = require('./routes/product')
 const auth = require('./routes/auth')
 const order = require('./routes/order')
-const category = require('./routes/category')
 const payment = require('./routes/payment')
+const category = require('./routes/category')
 
 app.use('/api/v1/',products)
+app.use('/api/v1',category)
 app.use('/api/v1/',auth)
 app.use('/api/v1/',order)
-app.use('/api/v1/',category)
 app.use('/api/v1/',payment)
 
 
